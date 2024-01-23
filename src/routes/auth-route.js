c469fabf0629433e6949e47f3abcb9accdb54f6b
Purachai200 <purashuyfiew20@gmail.com>
2024-01-23 10:29:24 +0700

    #

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth-controller");
 
-router.post("/auth/register", authController.register);
-router.post("/auth/login", authController.login);
-router.post("/auth/forget-password", authController.forgetPassword);
-router.get("/auth/forget-passwotd/:token", authController.verfyForgetPassword);
-router.post("/auth/reset-password", authController.resetPassword)
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/forget-password", authController.forgetPassword);
+router.get("/forget-passwotd/:token", authController.verfyForgetPassword);
+router.post("/reset-password", authController.resetPassword)
 
 module.exports = router;
