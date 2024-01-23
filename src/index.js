d6a9c322fbd0958d48ca83a92e5e8ff9f25fde3a
Purachai200 <purashuyfiew20@gmail.com>
2024-01-23 09:47:49 +0700

    #

--- src/index.js
+++ src/index.js
@@ -10,20 +10,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// app.get("/",(req, res, next) => {
+//     try {
+//         const { username, password } = req.body;
+    
+//         if(!username || !password) {
+//             return createError(400, "User and and Password must be Provided")
+//         }
+//     } catch (err) {
+//         next(err)
+//     };
+// });
+
 app.use(errorHandler);
 app.use("*", notFoundHandler);
-
-app.get("/",(req, res, next) => {
-    try {
-        const { username, password } = req.body;
-    
-        if(!username || !password) {
-            return createError(400, "User and and Password must be Provided")
-        }
-    } catch (err) {
-        next(err)
-    };
-});
 
 const port = process.env.PORT;
 app.listen(port, () => {
