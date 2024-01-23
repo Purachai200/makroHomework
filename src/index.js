9da7e7eecc6f660d95942e20b7708bfe3e48724f
Purachai200 <purashuyfiew20@gmail.com>
2024-01-23 09:29:22 +0700

    #

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const errorHandler = require('./middlewares/error');
+const notFoundHandler = require('./middlewares/notFound');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
+
+app.use(errorHandler);
+app.use("*", notFoundHandler);
 
 app.get("/",() => {});
 const port = process.env.PORT;
