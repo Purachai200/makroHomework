644b15afee3bc3f368029ba6bd452e7a105a1d31
Purachai200 <purashuyfiew20@gmail.com>
2024-01-23 09:45:52 +0700

    createError

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ const express = require("express");
 const cors = require("cors");
 const errorHandler = require('./middlewares/error');
 const notFoundHandler = require('./middlewares/notFound');
+const createError = require('./utils/createError');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler);
 
 app.use(errorHandler);
 app.use("*", notFoundHandler);
 
-app.get("/",() => {});
+app.get("/",(req, res, next) => {
+    try {
+        const { username, password } = req.body;
+    
+        if(!username || !password) {
+            return createError(400, "User and and Password must be Provided")
+        }
+    } catch (err) {
+        next(err)
+    };
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server run on port ` + port);
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -7,5 +7,5 @@ const errorHandler = (err, req, res, next) => {
   res
     .status(err.statusCode || 500)
     .json({ message: err.message || "Internal Server Error" });
-  module.exports = errorHandler;
 };
+module.exports = errorHandler;
